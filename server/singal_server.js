929a8fbe6153daab4d5167d1e87384d36c4895a2
tanziliang <963582312@qq.com>
2024-08-18 20:07:34 -0700

    ws

--- server/singal_server.js
+++ server/singal_server.js
@@ -5,6 +5,11 @@ var server = ws.createServer(function(conn)
 {
     console.log("创建新连接");
 
+    conn.on("text",function(str)
+    {
+        console.log("recv msg:"+str);
+    });
+
     conn.sendText("收到连接");
 
-});
+}).listen(port);
